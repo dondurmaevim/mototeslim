8a576184749885b652e40643d3e0ada9fe5503b4
dondurmaevim <39409662+dondurmaevim@users.noreply.github.com>
2026-03-08 00:45:48 +0300

    Update sms.js

--- api/sms.js
+++ api/sms.js
@@ -1,7 +1,7 @@
 // Vercel Serverless Function - /api/sms
 // Bu dosyayı GitHub'da /api/sms.js olarak yükle
 
-module.exports = async function handler handler(req, res) {
+module.exports = async function handler (req, res) {
   // CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
